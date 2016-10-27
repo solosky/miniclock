52c11e42dfb2b9481475540168c5334f7c0f8b85
solosky <solosky772@qq.com>
2016-10-27 20:29:12 +0800

    fix setting save bug

--- fw/src/setting.c
+++ fw/src/setting.c
@@ -4,15 +4,32 @@
 setting_data_t _setting_data EEMEM;
 
 result_t setting_read(setting_data_t* data){
-        eeprom_busy_wait();
-        eeprom_read_block(&_setting_data, data, sizeof(setting_data_t));
-        return ERR_OK;
+        return _setting_read_buff(&_setting_data, (byte_t*) data, sizeof(setting_data_t));
 }
 
 
 result_t setting_write(setting_data_t* data){
-        eeprom_busy_wait();
-        eeprom_write_block(&_setting_data, data, sizeof(setting_data_t));
-        printf("setting written!!%d\n", data->font_type);
+        return _setting_write_buff(&_setting_data,  (byte_t*) data, sizeof(setting_data_t));
+}
+
+
+result_t _setting_write_buff(uint8_t* base, byte_t* buff, uint8_t size){
+        cli(); // disable interrupt
+        for(uint8_t i=0; i<size; i++) {
+                eeprom_busy_wait();
+                eeprom_write_byte(base + i, buff[i]);
+        }
+        sei(); // enable interrupt
+        return ERR_OK;
+}
+
+
+result_t _setting_read_buff(uint8_t* base, byte_t* buff, uint8_t size){
+        cli(); // disable interrupt
+        for(uint8_t i=0; i<size; i++) {
+                eeprom_busy_wait();
+                buff[i] = eeprom_read_byte(base + i);
+        }
+        sei(); // enable interrupt
         return ERR_OK;
 }
--- fw/src/setting.h
+++ fw/src/setting.h
@@ -3,10 +3,18 @@
 
 #include "base.h"
 
+#define SETTING_MAX_ALARM_SIZE 2
+
+typedef struct _alarm_data_{
+    uint8_t hour;
+    uint8_t minute;
+    uint8_t repeat; // one bit represent a day
+} alarm_data_t;
 
 //setting info, store in eeprom
 typedef struct _setting_data_ {
         uint8_t font_type;
+        alarm_data_t alarms[SETTING_MAX_ALARM_SIZE];  //TODO need implements
 } setting_data_t;
 
 
@@ -15,5 +23,13 @@ typedef struct _setting_data_ {
 ///////////////////////////////////////////////////////////////////////////////
 result_t setting_read(setting_data_t* data);
 result_t setting_write(setting_data_t* data);
+
+
+///////////////////////////////////////////////////////////////////////////////
+// private functions
+///////////////////////////////////////////////////////////////////////////////
+result_t _setting_write_buff(uint8_t* base, byte_t* buff, uint8_t size);
+result_t _setting_read_buff(uint8_t* base, byte_t* buff, uint8_t size);
+
 
 #endif
